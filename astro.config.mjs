542ffcaa54b95411c19e1473a4a83de8a23fe975
Enzo Vivallo <enzovivallo@estudiante.uc.cl>
2025-08-10 23:43:22 -0400

    fix: remove pupetter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,8 @@
 import { defineConfig } from 'astro/config'
 import icon from 'astro-icon'
 import svelte from '@astrojs/svelte'
-import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   integrations: [icon(), svelte()],
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
 })

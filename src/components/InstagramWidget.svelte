f1985cae7c6af95d3b8e7cfe75802f5401bd15fd
Enzo Vivallo <enzovivallo@estudiante.uc.cl>
2025-08-11 00:48:07 -0400

    styling: instagram widget

--- src/components/InstagramWidget.svelte
+++ src/components/InstagramWidget.svelte
@@ -39,7 +39,7 @@
   class="instagram-media"
   data-instgrm-permalink="https://www.instagram.com/p/{id}/?utm_source=ig_embed&utm_campaign=loading"
   data-instgrm-version="14"
-  style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; width: 400px; min-width: 300px1 "
+  style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; width: 100%; max-width: 400px"
 >
   <div style="padding:16px;">
     <a

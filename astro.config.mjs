6e27729ecf3d01a107b08944550b38719d3f2989
Enzo Vivallo <enzovivallo@estudiante.uc.cl>
2025-08-10 21:44:01 -0400

    build: vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@
 import { defineConfig } from 'astro/config'
 import icon from 'astro-icon'
 import svelte from '@astrojs/svelte'
+import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   integrations: [icon(), svelte()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 })

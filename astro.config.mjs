c401a6798932f01f40a3128971d3e6c5c5ccbdd9
Enzo Vivallo <enzovivallo@estudiante.uc.cl>
2025-08-12 13:02:44 -0400

    build: remove svelte pkg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import icon from 'astro-icon'
-import svelte from '@astrojs/svelte'
 
 import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://academiajuvenil.progcomp.cl',
-  integrations: [icon(), svelte(), sitemap()],
+  integrations: [icon(), sitemap()],
 })

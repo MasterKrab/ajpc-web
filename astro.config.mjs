a3f9dfda29aeaf47f81b4e5e2515de2c84de0973
Enzo Vivallo <enzovivallo@estudiante.uc.cl>
2025-08-12 13:01:32 -0400

    build: add seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config'
 import icon from 'astro-icon'
 import svelte from '@astrojs/svelte'
 
+import sitemap from '@astrojs/sitemap'
+
 export default defineConfig({
-  integrations: [icon(), svelte()],
+  site: 'https://academiajuvenil.progcomp.cl',
+  integrations: [icon(), svelte(), sitemap()],
 })

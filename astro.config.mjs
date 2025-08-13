2314d9cf32a08e452b91411ed2012a50a994d6cb
Enzo Vivallo <enzovivallo@estudiante.uc.cl>
2025-08-12 20:58:25 -0400

    fix: instagram links and typing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://academiajuvenil.progcomp.cl',
-  integrations: [icon(), sitemap()],
+  integrations: [sitemap(), icon()],
 })
